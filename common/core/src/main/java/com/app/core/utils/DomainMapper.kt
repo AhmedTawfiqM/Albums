37c3bfe168e38c00a3c3cb9016d49d2b6a8bddb5
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-04-28 01:32:34 +0200

    Add DomainMapper

---
+++ common/core/src/main/java/com/app/core/utils/DomainMapper.kt
@@ -0,0 +1,8 @@
+package com.app.core.utils
+
+interface DomainMapper <T, DomainModel>{
+
+    fun mapToDomainModel(model: T): DomainModel
+
+    fun mapFromDomainModel(domainModel: DomainModel): T
+}
