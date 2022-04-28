a94889c948765705982ce129844eeaf97d7b8839
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-04-28 20:29:06 +0200

    Create AlbumsListView

--- app/src/main/java/com/app/albums/screen/albums/AlbumsScreen.kt
+++ app/src/main/java/com/app/albums/screen/albums/AlbumsScreen.kt
@@ -1,11 +1,17 @@
 package com.app.albums.screen.albums
 
 import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -13,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.albums.navhost.NavHostArgument
 import com.app.albums.navhost.Screen
+import com.app.albums.screen.albums.view.AlbumsListView
 import com.app.core.domain.albums.model.Album
 import com.app.presentation.activity.screen.AppScreen
 import com.app.presentation.activity.screen.ScreenHost
@@ -30,10 +37,36 @@ class AlbumsScreen(
     override fun Content() {
         LoadData()
 
-        Column {
+        Column(modifier = Modifier.padding(10.dp)) {
             ProfileTextView()
             UserDetailsView()
+            MyAlbumsTextView()
+
+            AlbumsListView(vm.albums).Content{
+                onAlbumClick(it)
+            }
         }
+    }
+
+    @Composable
+    private fun LoadData() {
+        LaunchedEffect(true) {
+            vm.fetchUsers {
+                vm.fetchAlbums()
+            }
+        }
+    }
+
+    @Composable
+    private fun MyAlbumsTextView() {
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "My Albums", //TODO : localize
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
     }
 
     @Composable
@@ -42,14 +75,15 @@ class AlbumsScreen(
         Column {
             Text(
                 text = vm.currentUser!!.name,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black,
+                fontSize = 15.sp
             )
             Spacer(modifier = Modifier.height(10.dp))
             Text(
                 text = vm.currentUser!!.addressDetails,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
+                color = Color.Black,
+                fontSize = 13.sp
             )
         }
     }
@@ -60,19 +94,13 @@ class AlbumsScreen(
             text = "Profile", //TODO : localize
             fontWeight = FontWeight.Bold,
             color = Color.Black,
-            fontSize = 15.sp
+            fontSize = 25.sp
         )
+        Spacer(modifier = Modifier.height(10.dp))
     }
 
-    private fun onAlbumClick() {
-        NavHostArgument.setValue(Album(1))
+    private fun onAlbumClick(album: Album) {
+        NavHostArgument.setValue(album)
         navigate(Screen.AlbumDetails)
-    }
-
-    @Composable
-    private fun LoadData() {
-        vm.fetchUsers {
-            vm.fetchAlbums()
-        }
     }
 }
---
+++ app/src/main/java/com/app/albums/screen/albums/view/AlbumsListView.kt
@@ -0,0 +1,47 @@
+package com.app.albums.screen.albums.view
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.core.domain.albums.model.Album
+
+class AlbumsListView(val albums: List<Album>) {
+
+    @Composable
+    fun Content(onAlbumClick: (Album) -> Unit) {
+        LazyColumn {
+            items(albums) {
+                AlbumItem(it, onAlbumClick)
+            }
+        }
+    }
+
+    @Composable
+    private fun AlbumItem(
+        album: Album,
+        onAlbumClick: (Album) -> Unit
+    ) {
+        Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+        Text(
+            modifier = Modifier.clickable {
+                onAlbumClick(album)
+            },
+            text = album.title ?: "",
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Black,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+    }
+
+}
