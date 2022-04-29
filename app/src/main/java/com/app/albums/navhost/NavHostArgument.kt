eccb8157cd7e3e4d77cb06c28af198e3c8f1b803
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-04-29 20:04:18 +0200

    Optimize NavHostArgument

--- app/src/main/java/com/app/albums/navhost/NavHostArgument.kt
+++ app/src/main/java/com/app/albums/navhost/NavHostArgument.kt
@@ -3,9 +3,9 @@ package com.app.albums.navhost
 import com.google.gson.Gson
 
 /**
-this class is Responsible to has data between Nav Screens.
-The problem with navigation compose is having magic strings in Route
-and not Reliable to pass Parcelable Objects
+this class is Responsible to pass data between Nav Screens.
+'The problem' with navigation compose is having magic strings in Route
+and not Reliable to pass Parcelable Objects.
  **/
 
 object NavHostArgument {
